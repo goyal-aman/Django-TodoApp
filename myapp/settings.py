99b1f772ff44a2ebd9e0a7f15f88aaca646153d6
goyal-aman <47478778+goyal-aman@users.noreply.github.com>
2019-10-14 18:04:59 +0530

    added profile page, added login required for profile page

--- myapp/settings.py
+++ myapp/settings.py
@@ -125,3 +125,4 @@ STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.register, name="users-register"),
+    path("register/", views.register, name="users-register"),
+    path("profile/", views.profile, name="users-profile"),
+    
     
 ]
