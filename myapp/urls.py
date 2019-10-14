138b3dfcaef44d3277453171af378e63d6cbaa28
goyal-aman <47478778+goyal-aman@users.noreply.github.com>
2019-10-14 16:24:09 +0530

    added login page - non fincitonal yet

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from todo.views import home, addTodo, deleteTodo, editTodo
-
+from django.contrib.auth import views as auth_views
 from users import views as users_views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     
     path("", home, name="home"),
     path("users/", include('users.urls')),
     
+    path("login/", auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    
+
     path("addTodo/", addTodo, name="addTodo"),
     path("delete/<int:item_id>/", deleteTodo, name="addTodo"),
     path("edit/<int:item_id>/", editTodo, name="editTodo"),
