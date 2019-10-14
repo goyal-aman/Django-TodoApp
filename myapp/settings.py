4c9d042d3d0aeed0cdd23e5bfb12b1e84b2495f7
goyal-aman <47478778+goyal-aman@users.noreply.github.com>
2019-10-14 15:09:48 +0530

    user register working

--- myapp/settings.py
+++ myapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'users',
     'todo',
     'django.contrib.admin',
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
