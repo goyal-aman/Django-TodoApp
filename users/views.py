039f4d6784e81787949698c4457813fde90f90e1
goyal-aman <47478778+goyal-aman@users.noreply.github.com>
2019-10-19 17:04:06 +0530

    added functionality to update user info

--- users/views.py
+++ users/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
 
+
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username=form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created')
-            return redirect('login')
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Your account has been created")
+            return redirect("login")
 
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, "users/register.html", {"form": form})
+
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request, f"Your account Info has been Updated")
+            return redirect('home')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+    context = {"u_form": u_form}
+    return render(request, "users/profile.html", context)
+
