d5dc24d852034b7164af04f59161b916fbf02963
goyal-aman <47478778+goyal-aman@users.noreply.github.com>
2019-10-19 13:42:08 +0530

    added title to todo model

--- todo/models.py
+++ todo/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
+    title = models.CharField(max_length=15,blank=True)
     content = models.TextField(default='')
     
